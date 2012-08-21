0c39d29d1325759ae9e50e55236570462c9015d6
James Kimmell <kimmellj@gmail.com>
2012-08-21 13:47:38 -0400

    Updating scripts for vagrant

--- recipes/package.rb
+++ recipes/package.rb
@@ -24,7 +24,7 @@ pkgs = value_for_platform(
     'default' => %w(php php-devel php-cli php-pear)
   },
   [ "debian", "ubuntu" ] => {
-    "default" => %w{ php5-cgi php5 php5-dev php5-cli php-pear }
+    "default" => %w{ php5-cgi php5 php5-dev php5-cli php-pear php5-mysql }
   },
   "default" => %w{ php5-cgi php5 php5-dev php5-cli php-pear }
 )
